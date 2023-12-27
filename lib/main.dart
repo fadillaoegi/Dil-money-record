f6e72cfb770131ea8da16cb3b1d999d3f2204405
fadillaoegi <fadilfdl2710@gmail.com>
2023-12-28 03:26:19 +0700

    update route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:dilrecord_money/routes/routes.dart';
+import 'package:dilrecord_money/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() => runApp(const MainApp());
 
@@ -7,6 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      getPages: [
+        GetPage(
+          name: RouteScreen.home,
+          page: () => const HomeScreen(),
+        )
+      ],
+    );
   }
 }
---
+++ lib/routes/routes.dart
@@ -0,0 +1,8 @@
+class RouteScreen {
+  static String root = "/DilRecordMoney";
+  static String register = "$root/register";
+  static String login = "$root/login";
+  static String detail = "$root/detail";
+  static String add = "$root/add";
+  static String home = "$root/home";
+}
